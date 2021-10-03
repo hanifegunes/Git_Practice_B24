3a5134902269bae74e5fa9b686a6fc5022018330
hanifegunes <hanifegunes@yahoo.com>
2021-10-03 15:35:56 -0500

    added wooden 1 spoon test

--- src/tests/WoodenSpoonTest.java
+++ src/tests/WoodenSpoonTest.java
@@ -3,7 +3,7 @@ package tests;
 public class WoodenSpoonTest {
     public static void main(String[] args) {
         for(int i = 0; i< 5; i++){
-            System.out.println("Wooden Spoon");
+            System.out.println("Wooden Spoons");
         }
     }
 }
